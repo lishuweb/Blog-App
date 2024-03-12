b80627dc4753bd7b7f9b67fafe72d7a1a36a5b42
Lishu Maharjan <maharjanlishu@gmail.com>
2024-03-12 15:54:58 +0545

    feat: token-functionalities-implemented

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,45 +1,44 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import * as dotenv from "dotenv";
 dotenv.config();
-import { NextFunction, Request, Response } from 'express';
 
 export const generateToken = (userData: any) => {
     if(!process.env.ACCESS_TOKEN_SECRET)
     {
         throw new Error ("Access Token Secret is not defined.")
     }
-    const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+    const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '3m'
+    });
     return accessToken;
 };
 
-export const verifyToken = (token: string) => {
-    try{
-        const isValid = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET || "") as JwtPayload;
-        return isValid;
-    }
-    catch(error)
+export const generateRefreshToken = (userData: any) => {
+    if(!process.env.REFRESH_TOKEN_SECRET)
     {
-        throw new Error ("Invalid Token!");
+        throw new Error ("Refresh Token Secret is not defined.");
     }
+    const refreshToken = jwt.sign(userData, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: '1h'
+    });
+    return refreshToken;
 };
 
-export const tokenExtractor = async (req: Request, res: Response, next: NextFunction) => {
-    if(!req.headers?.authorization)
-    {
-        res.status(401).json({
-            error: "Token Missing!"
-        });
-    }
-    const token = req.headers?.authorization?.split(" ")[1];
-    console.log(token, "TOKEN");
-    const isValid = verifyToken(token || "");
-    if(!isValid)
-    {
-        throw new Error ("Token invalid");
-    }
-    req.body.userId = isValid.id;
-    // req.token = token;
-    next();
+export const verifyToken = (token: string) => {
+    try {
+        return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET || "") as JwtPayload;
+      } catch (error) {
+        return null;
+      }
 };
+
+export const verifyRefreshToken = (refreshToken: string) => {
+    try {
+        return jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET || "") as JwtPayload;
+      } catch (error) {
+        return null;
+      }
+};
+
 
 
